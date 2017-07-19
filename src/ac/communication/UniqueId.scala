a97a12ec7f2ad0dfe7ea310a158e8c8ec4f23c47
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2017-07-19 18:16:33 +0300

    Remove obsolete communication abstraction

--- src/ac/communication/UniqueId.scala
+++
@@ -1,8 +0,0 @@
-package ac.communication
-
-import simulacrum.typeclass
-
-
-@typeclass trait UniqueId[F[_]] {
-  def generate: F[Int]
-}
--- src/ac/webapp/package.scala
+++ src/ac/webapp/package.scala
@@ -17,22 +17,9 @@ import monix.execution.Scheduler
 import monix.execution.atomic.AtomicInt
 
 package object webapp {
-  implicit object TaskToObservable extends Collectable[Observable, Task] {
-    override def collectFirstM[A, B](o: Observable[A])(f: PartialFunction[A, B]): Task[B] =
-      o.collect(f).headL
-
-    override def collectM_[A, B](o: Observable[A])(f: PartialFunction[A, Task[B]]): Task[Unit] =
-      o.collect(f).mapTask(identity).lastL.map(discard)
-  }
-
   implicit object ImpureRandomizer extends Randomizer[Task] {
     override def shuffles[A](v: TraversableOnce[A]): Task[Stream[A]] =
       Task.evalOnce { Stream.continually { Random.shuffle(v) }.flatten }
-  }
-
-  implicit object TaskUniqueId extends UniqueId[Task] {
-    private val counter = AtomicInt(0)
-    override def generate: Task[Int] = Task.eval { counter.incrementAndGet() }
   }
 
   def connect(
