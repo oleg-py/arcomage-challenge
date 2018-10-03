bd9b80bda09e72bf2f3e9d0bd0b5e8bb11838adb
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2018-10-03 23:20:00 +0300

    Allow actual selection of game conditions

--- frontend/src/main/scala/ac/frontend/pages/ConditionsSelectPage.scala
+++ frontend/src/main/scala/ac/frontend/pages/ConditionsSelectPage.scala
@@ -1,40 +1,71 @@
 package ac.frontend.pages
 
-import ac.frontend.Store
+import ac.frontend.{Store, Taverns}
 import ac.frontend.actions.connect
 import ac.frontend.components.PlayerDisplay
 import ac.frontend.states.AppState.User
-import ac.game.GameConditions
 import com.olegpy.shironeko.internals.SlinkyHotLoadingWorkaround._
-import slinky.core.Component
+import eu.timepit.refined.api.Refined
+import org.scalajs.dom.raw.HTMLSelectElement
+import slinky.core.{AttrPair, Component}
 import slinky.core.annotations.react
 import slinky.core.facade.ReactElement
 import slinky.web.html._
 
 @react class ConditionsSelectPage extends Component {
   case class Props(me: User, enemy: User)
-  type State = GameConditions
+  case class State(locationName: String, cards: Int)
 
-  def initialState: State = GameConditions.testing
+  def initialState: State = State(Taverns().head._1, 6)
 
   def render(): ReactElement = {
+    val conds = Taverns().apply(state.locationName)
+      .copy(handSize = Refined.unsafeApply(state.cards))
+
     div(className := "box wide")(
       PlayerDisplay(props.me),
       div(
-        div("Only standard conditions are currently supported"),
-        div(s"Cards: ${state.handSize}"),
-        div(s"Tower to win: ${state.victoryConditions.tower}"),
-        div(s"Resources to win: ${state.victoryConditions.resources}"),
+        div(
+          s"Cards: ",
+          select(
+            (value := state.cards.toString).asInstanceOf[AttrPair[select.tag.type]],
+            onChange := { e =>
+              val value = e.target.asInstanceOf[HTMLSelectElement].value.toInt
+              setState(_.copy(cards = value))
+            }
+          )(
+            option(value := "5")("5"),
+            option(value := "6")("6"),
+            option(value := "7")("7")
+          )
+        ),
+
+        select(
+          (value := state.locationName).asInstanceOf[AttrPair[select.tag.type]],
+          onChange := { e =>
+            val value = e.target.asInstanceOf[HTMLSelectElement].value
+            setState(_.copy(locationName = value))
+          }
+        )(
+          Taverns().map { case (tavernName, _) =>
+            option(
+              key := tavernName,
+              value := tavernName
+            )(tavernName)
+          }
+        ),
+        div(s"Tower to win: ${conds.victoryConditions.tower}"),
+        div(s"Resources to win: ${conds.victoryConditions.resources}"),
         hr(),
-        div(s"Tower: ${state.initialStats.buildings.tower.value}"),
-        div(s"Wall: ${state.initialStats.buildings.wall.value}"),
-        div(s"Income: ${state.initialStats.income.bricks.value}"),
-        div(s"Resources: ${state.initialStats.resources.bricks.value}"),
+        div(s"Tower: ${conds.initialStats.buildings.tower.value}"),
+        div(s"Wall: ${conds.initialStats.buildings.wall.value}"),
+        div(s"Income: ${conds.initialStats.income.bricks.value}"),
+        div(s"Resources: ${conds.initialStats.resources.bricks.value}"),
         div(className := "button-container-right")(
           button(
             className := "button",
             onClick := {() => Store.execS { implicit alg =>
-              connect.supplyConditions(state)
+              connect.supplyConditions(conds)
             }}
           )(s"Confirm")
         ),
