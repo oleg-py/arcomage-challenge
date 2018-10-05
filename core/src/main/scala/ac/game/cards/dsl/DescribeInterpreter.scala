ad500367e1edc697992f18068050e64c528c98f4
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2018-10-05 08:17:34 +0300

    Begin work on i18n

--- core/src/main/scala/ac/game/cards/dsl/DescribeInterpreter.scala
+++ core/src/main/scala/ac/game/cards/dsl/DescribeInterpreter.scala
@@ -12,59 +12,66 @@ import cats.syntax.foldable._
 import cats.instances.list._
 
 object DescribeInterpreter {
+  @deprecated("Use locale-specific version", "")
   def apply(dslEntry: DSLEntry): Chain[String] =
-    scheme.cata(algebra).apply(dslEntry)
+    en(dslEntry)
 
-  def stringify(comparison: Comparison): String = comparison match {
-    case GreaterThan => ">"
-    case EqualTo => "="
-    case LessThan => "<"
-  }
+  val en = scheme.cata(En.algebra)
 
-  def stringify(r: Read): String = r match {
-    case Val(value) => value.toString
-    case Var(target, prop) =>
-      val tgs = target match {
-        case structure.Player => "player"
-        case structure.Enemy => "enemy"
-      }
-
-      val ps = prop match {
-        case Income(Bricks) => "quarry"
-        case Income(Gems) => "magic"
-        case Income(Recruit) => "dungeon"
-        case Avail(Bricks) => "bricks"
-        case Avail(Gems) => "gems"
-        case Avail(Recruit) => "recruits"
-        case Wall => "wall"
-        case Tower => "tower"
-      }
-
-      tgs + " " + ps
-  }
-
-  def stringify(c: Condition): String =
-    stringify(c.x) ++ " " ++ stringify(c.op) ++ " " ++ stringify(c.other)
-
-  def algebra: Algebra[DSLEntryF, Chain[String]] =
-    Algebra {
-      case  PlayAgain => Chain("Play again")
-      case  SwapWalls => Chain("Swap walls")
-      case  DiscardCard => Chain("Receive a card", "Discard a card")
-      case  Assign(from, to) =>
-        Chain(s"${stringify(from)} becomes as ${stringify(to)}")
-      case  Modify(from, to) =>
-        val str = s"$to to ${stringify(from)}"
-        if (str startsWith "-") Chain(str)
-        else Chain("+" + str)
-      case  Damage(Player, value) =>
-        Chain(s"You receive ${stringify(value)} damage")
-      case  Damage(Enemy, value) =>
-        Chain(s"${stringify(value)} damage")
-      case  Alt(c, ifTrue, ifFalse) =>
-        val prefix = Chain(s"if ${stringify(c)}") ++ ifTrue
-        ifFalse.fold(prefix)(prefix.append("otherwise") ++ _)
-      case  Combination(as) =>
-        as.combineAll
+  private object En {
+    def stringify(comparison: Comparison): String = comparison match {
+      case GreaterThan => ">"
+      case EqualTo => "="
+      case LessThan => "<"
     }
+
+    def stringify(r: Read): String = r match {
+      case Val(value) => value.toString
+      case Var(target, prop) =>
+        val tgs = target match {
+          case structure.Player => "player"
+          case structure.Enemy => "enemy"
+        }
+
+        val ps = prop match {
+          case Income(Bricks) => "quarry"
+          case Income(Gems) => "magic"
+          case Income(Recruit) => "dungeon"
+          case Avail(Bricks) => "bricks"
+          case Avail(Gems) => "gems"
+          case Avail(Recruit) => "recruits"
+          case Wall => "wall"
+          case Tower => "tower"
+        }
+
+        tgs + " " + ps
+    }
+
+    def stringify(c: Condition): String =
+      stringify(c.x) ++ " " ++ stringify(c.op) ++ " " ++ stringify(c.other)
+
+    def algebra: Algebra[DSLEntryF, Chain[String]] =
+      Algebra {
+        case  PlayAgain => Chain("Play again")
+        case  SwapWalls => Chain("Swap walls")
+        case  DiscardCard => Chain("Receive a card", "Discard a card")
+        case  Assign(from, to) =>
+          Chain(s"${stringify(from)} becomes as ${stringify(to)}")
+        case  Modify(from, to) =>
+          val str = s"$to to ${stringify(from)}"
+          if (str startsWith "-") Chain(str)
+          else Chain("+" + str)
+        case  Damage(Player, value) =>
+          Chain(s"You receive ${stringify(value)} damage")
+        case  Damage(Enemy, value) =>
+          Chain(s"${stringify(value)} damage")
+        case  Alt(c, ifTrue, ifFalse) =>
+          val prefix = Chain(s"if ${stringify(c)}") ++ ifTrue
+          ifFalse.fold(prefix)(prefix.append("otherwise") ++ _)
+        case  Combination(as) =>
+          as.combineAll
+      }
+  }
+
+
 }
--- frontend/src/main/scala/ac/frontend/i18n/Lang.scala
+++ frontend/src/main/scala/ac/frontend/i18n/Lang.scala
@@ -1,12 +1,53 @@
 package ac.frontend.i18n
 
 import ac.game.cards.Card
+import ac.game.cards.Card.Color
+import ac.game.cards.dsl.DescribeInterpreter
 import ac.game.cards.dsl.structure.DSLEntry
 import cats.data.Chain
 
 
 trait Lang {
-  def cardName(str: String): String
-  def cardDescription(dslEntry: DSLEntry): Chain[String]
-  def resourceName(color: Card.Color): String
+  def cardName(str: String): String = str
+  def cardDescription(dslEntry: DSLEntry): Chain[String] =
+    DescribeInterpreter(dslEntry)
+
+  def resourceName(color: Card.Color): String = color match {
+    case Color.Red   => "Bricks"
+    case Color.Blue  => "Gems"
+    case Color.Green => "Recruits"
+  }
+
+  def sNickname  = "Nickname"
+  def sEmail     = "Email (optional - for avatar only)"
+  def sEnterGame = "Enter a game"
+
+  def sShareLink = "Share this link with your friend to start a game"
+
+  def sCards      = "Cards"
+  def sTowerToWin = "Tower to win"
+  def sResToWin   = "Resources to win"
+  def sTower      = "Tower"
+  def sWall       = "Wall"
+  def sIncome     = "Income"
+  def sResources  = "Resources"
+  def sConfirm    = "Confirm"
+}
+
+object Lang {
+  object En extends Lang
+
+  object Ru extends Lang {
+    override def resourceName(color: Color): String = color match {
+      case Color.Red   => "Кирпичи"
+      case Color.Blue  => "Драгоценности"
+      case Color.Green => "Звери"
+    }
+
+    override def sNickname  = "Имя"
+    override def sEmail     = "Email (опционально, только для аватара)"
+    override def sEnterGame = "Войти в игру"
+
+    override def sShareLink = "Поделитесь ссылкой с другом для начала игры"
+  }
 }
