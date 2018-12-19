e3ec86f80c980efffde91791f2eddca509105fef
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2018-12-19 17:23:11 +0300

    feat: disable <Dummy> placeholder in prod

--- frontend/src/main/scala/ac/frontend/components/DummyCards.scala
+++ frontend/src/main/scala/ac/frontend/components/DummyCards.scala
@@ -1,5 +1,6 @@
 package ac.frontend.components
 
+import ac.frontend.utils
 import slinky.core.StatelessComponent
 import slinky.core.annotations.react
 import slinky.core.facade.ReactElement
@@ -9,5 +10,6 @@ import slinky.web.html.{className, div}
 @react class DummyCards extends StatelessComponent {
   type Props = Unit
 
-  def render(): ReactElement = div(className := "dummy-cards")("<Dummy>")
+  def render(): ReactElement =
+    div(className := "dummy-cards")(if (utils.isDevelopment) "<Dummy>" else " ")
 }
