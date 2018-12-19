a6a25c953324b40707f24899c852723a25e3a6d8
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2018-12-20 00:07:41 +0300

    feat: tooltips for resource/income names

--- frontend/src/main/scala/ac/frontend/components/ResourceBox.scala
+++ frontend/src/main/scala/ac/frontend/components/ResourceBox.scala
@@ -1,22 +1,28 @@
 package ac.frontend.components
 
 import ac.game.cards.Card
+import ac.game.cards.Card.Color
 import eu.timepit.refined.types.numeric.{NonNegInt, PosInt}
 import slinky.core.StatelessComponent
 import slinky.core.annotations.react
 import slinky.core.facade.ReactElement
-import slinky.web.html.{className, div, span}
+import slinky.web.html.{className, div, span, title}
 
 
 @react class ResourceBox extends StatelessComponent {
   case class Props(tpe: Card.Color, res: NonNegInt, income: PosInt, max: PosInt)
 
   def render(): ReactElement = {
+    val (incomeName, amountName) = props.tpe match {
+      case Color.Red => ("Quarry","Bricks")
+      case Color.Blue => ("Magic", "Gems")
+      case Color.Green => ("Dungeon", "Recruits")
+    }
     val colorClass = props.tpe.lowerName
 
     div(className := s"resource-box $colorClass")(
-      div(className := "income")(props.income.toString),
-      div(className := "amount")(
+      div(className := "income", title := incomeName)(props.income.toString),
+      div(className := "amount", title := amountName)(
         span(props.res.toString),
         span(className := "additional-info")(s"/${props.max}")
       )
